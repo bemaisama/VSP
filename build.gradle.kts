35c10be64f40d3da0433d7ecdb8e36928534ce88
Mario <malcala45@gmail.com>
2024-02-22 06:20:42 -0500

    esta bien

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
-    }
-}
+
+
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
