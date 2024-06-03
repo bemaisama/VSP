86fea7e7d20dbabcb0eb3d8a0f7d029d1412dc62
Mario <malcala45@gmail.com>
2024-06-03 09:41:08 -0500

    v 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 5
-        versionName = "1.3.2"
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
