84cf50c8a0a407acd6464bbde90c918507efa34e
Mario <malcala45@gmail.com>
2024-06-07 22:00:13 -0500

    v 1.4.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.vidaensupalabra.vsp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.4.5"
+        versionCode = 12
+        versionName = "1.4.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
